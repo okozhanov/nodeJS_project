cbe5faddabb9edc445044e694cc1743d834005fb
okozhanov <aleks.kozhanov.kr@gmail.com>
2021-10-05 19:35:56 +0300

    classwork_1 trying to fix

--- app.js
+++ app.js
@@ -1,68 +1,57 @@
-const async = require('async')
 const fs = require('fs')
 const path = require('path')
 const users = require(path.join(__dirname, 'users.js'))
 
-// const dirCreator = (name) => {
-//
-// }
+const menOldFolder = 'menOlderThen20'
+const womenOldFolder = 'womenOlderThen20'
+const menYoungFolder = 'menYoungerThen20'
+const womenYoungFolder = 'womenYoungerThen20'
 
+async function writer(destination, user) {
+    let name = user.name + '.txt'
 
-fs.mkdir(path.join(__dirname, 'menOlderThen20'),
-    {recursive: true},
-    (err) => {
-    if (err) {
-        console.log(err);
-    }
-})
+    await fs.writeFile(
+        path.join(destination, name),
+        JSON.stringify(user),
+        err => {
+            if (err) {
+                console.log(err)
+            }
+        }
+    )
+}
 
+async function dirCreator(name) {
+    await fs.mkdir(path.join(__dirname, name),
+        {recursive: true},
+        (err) => {
+            if (err) {
+                console.log(err);
+            }
+        })
+    return path.join(__dirname, name)
+}
 
+async function judge(users) {
+    await users.forEach(user => {
+        if (user.gender === 'female') {
+            if (user.age >= 20) {
+                writer(dirCreator(womenOldFolder), user)
+            }
+            if (user.age < 20) {
+                writer(dirCreator(womenYoungFolder), user)
+            }
+        }
 
-// dirCreator('menOlderThen20')
-// dirCreator('womenOlderThen20')
-// dirCreator('menYoungerThen20')
-// dirCreator('womenYoungerThen20')
-//
-// const menOldFolder = path.join(__dirname, 'menOlderThen20')
-// const womenOldFolder = path.join(__dirname, 'womenOlderThen20')
-// const menYoungFolder = path.join(__dirname, 'menYoungerThen20')
-// const womenYoungFolder = path.join(__dirname, 'womenYoungerThen20')
-//
-// const writer = (destination, user) => {
-//     let name = user.name + '.txt'
-//
-//     fs.writeFile(
-//         path.join(destination, name),
-//         JSON.stringify(user),
-//         err => {
-//             if (err) {
-//                 console.log(err)
-//             }
-//         }
-//     )
-// }
-//
-//
-// const judge = (users) => {
-//     users.forEach(user => {
-//         if (user.gender === 'female') {
-//             if (user.age >= 20) {
-//                 writer(womenOldFolder, user)
-//             }
-//             if (user.age < 20) {
-//                 writer(womenYoungFolder, user)
-//             }
-//         }
-//
-//         if (user.gender === 'male') {
-//             if (user.age >= 20) {
-//                 writer(menOldFolder, user)
-//             }
-//             if (user.age < 20) {
-//                 writer(menYoungFolder, user)
-//             }
-//         }
-//     })
-// }
-//
-// judge(users)
+        if (user.gender === 'male') {
+            if (user.age >= 20) {
+                writer(dirCreator(menOldFolder), user)
+            }
+            if (user.age < 20) {
+                writer(dirCreator(menYoungFolder), user)
+            }
+        }
+    })
+}
+
+judge(users)
