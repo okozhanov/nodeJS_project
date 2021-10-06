d11fbe30777db7e01bf9f89954f90b75096b7274
okozhanov <aleks.kozhanov.kr@gmail.com>
2021-10-06 14:58:02 +0300

    homework_1 fixed

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-const fs = require('fs')
-const path = require('path')
+const fs = require('fs');
+const path = require('path');
 
-const boysPath = path.join(__dirname, 'boys')
-const girlsPath = path.join(__dirname, 'girls')
+const boysPath = path.join(__dirname, 'boys');
+const girlsPath = path.join(__dirname, 'girls');
 
 const mover = (folder, file, fileName, destination) => {
 
@@ -11,46 +11,42 @@ const mover = (folder, file, fileName, destination) => {
         path.join(destination, fileName),
         (err) => {
             if (err) {
-                console.log(err)
+                console.log(err);
             }
         }
-    )
+    );
 }
-
 
 const myFunction = (pathForDirectory) => {
     fs.readdir(pathForDirectory, (err, data) => {
         if (err) {
-            console.log(err)
+            console.log(err);
             return;
         }
 
         data.forEach(fileName => {
-            let thisFile = path.join(pathForDirectory, fileName)
+            const thisFile = path.join(pathForDirectory, fileName)
 
             fs.readFile(thisFile, ((err, data) => {
                 if (err) {
                     console.log(err);
-                    return
+                    return;
                 }
 
-                object = JSON.parse(data.toString())
+                const object = JSON.parse(data.toString());
 
-                if (pathForDirectory === boysPath) {
-                    if (object.gender === 'female') {
-                        mover(pathForDirectory, thisFile, fileName, girlsPath)
-                    }
+                if (pathForDirectory === boysPath && object.gender === 'female') {
+                    mover(pathForDirectory, thisFile, fileName, girlsPath);
+                    return;
                 }
 
-                if (pathForDirectory === girlsPath) {
-                    if (object.gender === 'male') {
-                        mover(pathForDirectory, thisFile, fileName, boysPath)
-                    }
+                if (pathForDirectory === girlsPath && object.gender === 'male') {
+                    mover(pathForDirectory, thisFile, fileName, boysPath);
                 }
-            }))
-        })
-    })
+            }));
+        });
+    });
 }
 
 myFunction(boysPath);
-myFunction(girlsPath)
+myFunction(girlsPath);
